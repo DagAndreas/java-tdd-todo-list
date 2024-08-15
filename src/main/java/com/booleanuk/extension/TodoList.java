1a7bad331ca4e33051c8b56cd3f93f28d3700849
Dag Andreas <daanfofo@gmail.com>
2024-08-15 13:24:35 +0200

    added removetask

--- src/main/java/com/booleanuk/extension/TodoList.java
+++ src/main/java/com/booleanuk/extension/TodoList.java
@@ -5,8 +5,12 @@ import java.util.HashMap;
 public class TodoList  {
     HashMap<Integer, Task> tasks = new HashMap<>();
 
-    public void add(int id, String task){
+    public void addTask(int id, String task){
         Task newTask = new Task(task);
         tasks.put(id, newTask);
+    }
+
+    public void removeTask(int id){
+        tasks.remove(id);
     }
 }
