ed9dac0273b4afea377b84c5d64a5c9709ffb848
Dag Andreas <daanfofo@gmail.com>
2024-08-15 09:07:10 +0200

    ready for testing

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,10 @@
 package com.booleanuk.core;
 
+import javafx.util.Pair;
+import java.util.ArrayList;
+
 public class TodoList {
+    private ArrayList<Pair<String, Boolean>> tasks = new ArrayList<>();
+
 
 }
