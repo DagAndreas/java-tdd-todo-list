ea3998cbb2b88002de3a0d84853101a5d924739c
Dag Andreas <daanfofo@gmail.com>
2024-08-15 13:37:28 +0200

    added seeAllTasks test

--- src/test/java/com/booleanuk/extension/TestTodoList.java
+++ src/test/java/com/booleanuk/extension/TestTodoList.java
@@ -3,6 +3,9 @@ package com.booleanuk.extension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TestTodoList {
 
     @Test
@@ -28,6 +31,25 @@ public class TestTodoList {
         Assertions.assertEquals(td.tasks.get(1).task, "Bake cake");
         td.removeTask(1);
         Assertions.assertNull(td.tasks.get(1));
+
+    }
+
+
+    @Test
+    public void TestseeAllTasks(){
+        TodoList td = new TodoList();
+        td.addTask(0, "Clean");
+        td.addTask(1, "Wash car");
+        td.addTask(2, "Clean again");
+
+        ArrayList<String> expectedResults = new ArrayList<>();
+        expectedResults.add("Clean"); expectedResults.add("Clean again"); expectedResults.add("Wash car");
+
+        HashMap<Integer, Task> gottenResults = td.seeAllTasks();
+        for (Task task: gottenResults.values()){
+            boolean containedInExpectedResults = expectedResults.contains(task.task);
+            Assertions.assertTrue(containedInExpectedResults);
+        }
 
     }
 }
