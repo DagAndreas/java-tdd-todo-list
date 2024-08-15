e3e9cb75e1dfe91f695719f0b76b359e71252fe4
Dag Andreas <daanfofo@gmail.com>
2024-08-15 09:21:03 +0200

    added more tests to addTask

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -18,6 +18,16 @@ class TodoListTest {
         TodoList todolist = new TodoList();
 
         todolist.addTask("Buy groceries");
+        todolist.addTask("Take out the trash");
+        todolist.addTask("Wash car");
         Assertions.assertTrue(todolist.tasks.contains(new Pair<>("Buy groceries", false)));
+        Assertions.assertTrue(todolist.tasks.contains(new Pair<>("Take out the trash", false)));
+        Assertions.assertFalse(todolist.tasks.contains(new Pair<>("Take in the trash", false)));
+        Assertions.assertTrue(todolist.tasks.contains(new Pair<>("Wash car", false)));
+        Assertions.assertFalse(todolist.tasks.contains(new Pair<>("Wash car again", false)));
+    }
+
+    public void testMarkTask(){
+
     }
 }
