9b121ead5ae972a1c68dabfd3a0334b1d3f7b15a
Dag Andreas <daanfofo@gmail.com>
2024-08-15 13:24:27 +0200

    added testremovetask

--- src/test/java/com/booleanuk/extension/TestTodoList.java
+++ src/test/java/com/booleanuk/extension/TestTodoList.java
@@ -10,7 +10,24 @@ public class TestTodoList {
         TodoList td = new TodoList();
         Task testTask = new Task("Clean");
 
-        td.add(0, "Clean");
+        td.addTask(0, "Clean");
         Assertions.assertEquals(td.tasks.get(0).task, "Clean");
+    }
+
+    @Test
+    public void TestRemoveTask(){
+        TodoList td = new TodoList();
+        td.addTask(0, "Clean");
+        td.addTask(1, "Bake cake");
+
+        Assertions.assertEquals(td.tasks.get(0).task, "Clean");
+        td.removeTask(0);
+        Assertions.assertNull(td.tasks.get(0));
+
+
+        Assertions.assertEquals(td.tasks.get(1).task, "Bake cake");
+        td.removeTask(1);
+        Assertions.assertNull(td.tasks.get(1));
+
     }
 }
