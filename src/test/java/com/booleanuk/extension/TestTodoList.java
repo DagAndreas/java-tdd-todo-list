dda844120e7597fee6c7487c5d189755310a6867
Dag Andreas <daanfofo@gmail.com>
2024-08-15 14:48:46 +0200

    task asc order

--- src/test/java/com/booleanuk/extension/TestTodoList.java
+++ src/test/java/com/booleanuk/extension/TestTodoList.java
@@ -79,11 +79,32 @@ public class TestTodoList {
         Assertions.assertEquals(td.getTask(0), "Clean");
         Assertions.assertEquals(td.getTask(1), "Wash car");
         Assertions.assertEquals(td.getTask(2), "Clean again");
-        Assertions.assertNotEquals(td.getTask(4), "Clean again");
+        Assertions.assertNull(td.getTask(-4));
+    }
 
+
+    @Test
+    public void listTasksAscendingOrder(){
+        TodoList td = new TodoList();
+        td.addTask(0, "d");
+        td.addTask(1, "g");
+        td.addTask(2, "b");
+        td.addTask(3, "a");
+        td.addTask(4, "x");
+        td.addTask(5, "e");
+
+        ArrayList<Task> expectedOrder = new ArrayList<>();
+        String[] order = {"a", "b", "d", "e", "g", "x"};
+        for (String s: order){
+            expectedOrder.add(new Task(s));
+        }
+        ArrayList<Task> result = td.listTasksAscendingOrder();
+        for (int i = 0; i < expectedOrder.size(); i++) {
+            Assertions.assertEquals(result.get(i).task, expectedOrder.get(i).task);
+
+        }
 
 
     }
-
 
 }
